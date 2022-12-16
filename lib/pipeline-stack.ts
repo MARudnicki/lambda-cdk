e595b3850fe2d05af22764df3154a89e0d38a249
Maciej Rudnicki <maciejru@amazon.com>
2022-12-16 11:21:22 +0100

    Clean up

---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+
+export class MyPipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'TestPipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub('MARudnicki/lambda-cdk', 'main'), //Remember to change
+                commands: [
+                    'npm ci',
+                    'npx cdk synth'
+                ]
+            })
+        });
+    }
+}
